bc72002de637c1f4f239ec1482f2f90086f82ce7
Thui Sing <thuisingmong0@gmailcom>
2022-01-07 13:08:33 +0600

    added

--- src/Components/Pages/Cart/Cart.js
+++ src/Components/Pages/Cart/Cart.js
@@ -1,95 +1,95 @@
-import React, { useState } from "react";
-import useCart from "../../Hooks/useCart";
-import Footer from "../Shared/Footer/Footer";
-import Navbar from "../Shared/Navbar/Navbar";
-import axios from "axios";
-import useAuth from "../../Hooks/useAuth";
-import { Link } from "react-router-dom";
-import PurchasePopUp from "./PurchasePopUp/PurchasePopUp";
+// import React, { useState } from "react";
+// import useCart from "../../Hooks/useCart";
+// import Footer from "../Shared/Footer/Footer";
+// import Navbar from "../Shared/Navbar/Navbar";
+// import axios from "axios";
+// import useAuth from "../../Hooks/useAuth";
+// import { Link } from "react-router-dom";
+// import PurchasePopUp from "./PurchasePopUp/PurchasePopUp";
 
-const Cart = () => {
-  const [cart, setCart] = useCart();
-  const { user } = useAuth();
-  const [popup, setPopup] = useState(false);
-  const handleCross = (id) => {
-    const existed = cart.filter((item) => item._id !== id);
-    setCart(existed);
-    const newCart = {
-      addedCart: existed,
-      email: user.email,
-    };
-    axios
-      .put(`https://desolate-brushlands-67419.herokuapp.com/cart`, newCart)
-      .then((res) => {
-        console.log(res);
-      });
-    // console.log(id);
-  };
-  return (
-    <div>
-      <Navbar />
-      <div className="py-12 mt-28 container mx-auto ">
-        <h3 className="text-4xl font-medium mb-6">You Added on Cart : </h3>
-        <div className="grid md:grid-cols-2 gap-5">
-          {cart.length <= 0 ? (
-            <h2 className="text-xl font-medium text-red-500">
-              No item Added on cart .
-              <Link to="/explore">
-                <span className="text-blue-800 hover:underline p-2 transition-all">
-                  Add cart Now
-                </span>
-              </Link>
-            </h2>
-          ) : (
-            cart.map((item, ind) => (
-              <div key={ind} className="flex items-center">
-                <img
-                  className="w-1/3"
-                  src={`data:image/jpeg;base64,${item.img}`}
-                  alt=""
-                />
-                <div className="flex items-center">
-                  <div>
-                    <h3 className="font-medium text-2xl">Name : {item.name}</h3>
-                    <h3 className="font-medium">
-                      Price : {item.price * item.quantity}
-                    </h3>
-                    <h3 className="font-medium"> Quantity : {item.quantity}</h3>
-                  </div>
-                  <div onClick={() => handleCross(item._id)}>
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      className="h-6 w-6 bg-red-400 text-red-800 ml-3 cursor-pointer"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke="currentColor"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth={2}
-                        d="M6 18L18 6M6 6l12 12"
-                      />
-                    </svg>
-                  </div>
-                </div>
-              </div>
-            ))
-          )}
-        </div>
-        <div className="text-right">
-          <button
-            onClick={() => (popup ? setPopup(false) : setPopup(true))}
-            className="text-white bg-gray-800 px-5 py-3 mt-10"
-          >
-            Order All Now
-          </button>
-        </div>
-      </div>
-      {popup && <PurchasePopUp setPopup={setPopup} />}
-      <Footer />
-    </div>
-  );
-};
+// const Cart = () => {
+//   const [cart, setCart] = useCart();
+//   const { user } = useAuth();
+//   const [popup, setPopup] = useState(false);
+//   const handleCross = (id) => {
+//     const existed = cart.filter((item) => item._id !== id);
+//     setCart(existed);
+//     const newCart = {
+//       addedCart: existed,
+//       email: user.email,
+//     };
+//     axios
+//       .put(`https://desolate-brushlands-67419.herokuapp.com/cart`, newCart)
+//       .then((res) => {
+//         console.log(res);
+//       });
+//     // console.log(id);
+//   };
+//   return (
+//     <div>
+//       <Navbar />
+//       <div className="py-12 mt-28 container mx-auto ">
+//         <h3 className="text-4xl font-medium mb-6">You Added on Cart : </h3>
+//         <div className="grid md:grid-cols-2 gap-5">
+//           {cart.length <= 0 ? (
+//             <h2 className="text-xl font-medium text-red-500">
+//               No item Added on cart .
+//               <Link to="/explore">
+//                 <span className="text-blue-800 hover:underline p-2 transition-all">
+//                   Add cart Now
+//                 </span>
+//               </Link>
+//             </h2>
+//           ) : (
+//             cart.map((item, ind) => (
+//               <div key={ind} className="flex items-center">
+//                 <img
+//                   className="w-1/3"
+//                   src={`data:image/jpeg;base64,${item.img}`}
+//                   alt=""
+//                 />
+//                 <div className="flex items-center">
+//                   <div>
+//                     <h3 className="font-medium text-2xl">Name : {item.name}</h3>
+//                     <h3 className="font-medium">
+//                       Price : {item.price * item.quantity}
+//                     </h3>
+//                     <h3 className="font-medium"> Quantity : {item.quantity}</h3>
+//                   </div>
+//                   <div onClick={() => handleCross(item._id)}>
+//                     <svg
+//                       xmlns="http://www.w3.org/2000/svg"
+//                       className="h-6 w-6 bg-red-400 text-red-800 ml-3 cursor-pointer"
+//                       fill="none"
+//                       viewBox="0 0 24 24"
+//                       stroke="currentColor"
+//                     >
+//                       <path
+//                         strokeLinecap="round"
+//                         strokeLinejoin="round"
+//                         strokeWidth={2}
+//                         d="M6 18L18 6M6 6l12 12"
+//                       />
+//                     </svg>
+//                   </div>
+//                 </div>
+//               </div>
+//             ))
+//           )}
+//         </div>
+//         <div className="text-right">
+//           <button
+//             onClick={() => (popup ? setPopup(false) : setPopup(true))}
+//             className="text-white bg-gray-800 px-5 py-3 mt-10"
+//           >
+//             Order All Now
+//           </button>
+//         </div>
+//       </div>
+//       {popup && <PurchasePopUp setPopup={setPopup} />}
+//       <Footer />
+//     </div>
+//   );
+// };
 
-export default Cart;
+// export default Cart;
--- src/Components/Pages/Cart/PurchasePopUp/PurchasePopUp.js
+++ src/Components/Pages/Cart/PurchasePopUp/PurchasePopUp.js
@@ -1,135 +1,135 @@
-import axios from "axios";
-import React from "react";
-import { useForm } from "react-hook-form";
-import { useNavigate } from "react-router-dom";
-import useAuth from "../../../Hooks/useAuth";
-import useCart from "../../../Hooks/useCart";
+// import axios from "axios";
+// import React from "react";
+// import { useForm } from "react-hook-form";
+// import { useNavigate } from "react-router-dom";
+// import useAuth from "../../../Hooks/useAuth";
+// import useCart from "../../../Hooks/useCart";
 
-const PurchasePopUp = ({ setPopup }) => {
-  const { user } = useAuth();
-  const [cart, setCart] = useCart();
-  const navigate = useNavigate();
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm();
-  const date = new Date();
+// const PurchasePopUp = ({ setPopup }) => {
+//   const { user } = useAuth();
+//   const [cart, setCart] = useCart();
+//   const navigate = useNavigate();
+//   const {
+//     register,
+//     handleSubmit,
+//     formState: { errors },
+//   } = useForm();
+//   const date = new Date();
 
-  const onSubmit = (data) => {
-    const newCart = cart.map((element) => {
-      element.email = user.email;
-      element.status = "pending";
-      element.address = data.address;
-      element.address = data.address;
-      element.number = data.number;
-      element.date = date.toLocaleDateString();
-      delete element._id;
-      return element;
-    });
-    axios
-      .post("https://desolate-brushlands-67419.herokuapp.com/orders", newCart)
-      .then((res) => {
-        if (res.data.insertedCount > 0) {
-          alert("ordered successfully.please pay now");
-          axios
-            .delete(
-              `https://desolate-brushlands-67419.herokuapp.com/cart/${user.email}`
-            )
-            .then((res) => {
-              if (res.data.deletedCount > 0) {
-                setCart([]);
-                navigate("/dashboard/my-orders");
-              }
-            });
-        }
-      });
-  };
-  return (
-    <div className="absolute w-full h-screen bg-primary  top-0 left-0 z-50 flex justify-center items-center">
-      <div className="bg-white w-1/2 p-6 relative">
-        <span
-          onClick={() => setPopup(false)}
-          className="absolute top-2 right-3 bg-red-300 rounded hover:text-red-700 bg-red-100"
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-6 w-6"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth={2}
-              d="M6 18L18 6M6 6l12 12"
-            />
-          </svg>
-        </span>
-        <form onSubmit={handleSubmit(onSubmit)} className="space-y-5 my-4">
-          <div>
-            <label className="text-xl " htmlFor="name">
-              Name
-            </label>
-            <input
-              className="w-full mt-1 p-3 border-2 border-gray-400 rounded outline-none"
-              id="name"
-              defaultValue={user.displayName}
-              {...register("name", { required: true })}
-            />
-          </div>
+//   const onSubmit = (data) => {
+//     const newCart = cart.map((element) => {
+//       element.email = user.email;
+//       element.status = "pending";
+//       element.address = data.address;
+//       element.address = data.address;
+//       element.number = data.number;
+//       element.date = date.toLocaleDateString();
+//       delete element._id;
+//       return element;
+//     });
+//     axios
+//       .post("https://desolate-brushlands-67419.herokuapp.com/orders", newCart)
+//       .then((res) => {
+//         if (res.data.insertedCount > 0) {
+//           alert("ordered successfully.please pay now");
+//           axios
+//             .delete(
+//               `https://desolate-brushlands-67419.herokuapp.com/cart/${user.email}`
+//             )
+//             .then((res) => {
+//               if (res.data.deletedCount > 0) {
+//                 setCart([]);
+//                 navigate("/dashboard/my-orders");
+//               }
+//             });
+//         }
+//       });
+//   };
+//   return (
+//     <div className="absolute w-full h-screen bg-primary  top-0 left-0 z-50 flex justify-center items-center">
+//       <div className="bg-white w-1/2 p-6 relative">
+//         <span
+//           onClick={() => setPopup(false)}
+//           className="absolute top-2 right-3 bg-red-300 rounded hover:text-red-700 bg-red-100"
+//         >
+//           <svg
+//             xmlns="http://www.w3.org/2000/svg"
+//             className="h-6 w-6"
+//             fill="none"
+//             viewBox="0 0 24 24"
+//             stroke="currentColor"
+//           >
+//             <path
+//               strokeLinecap="round"
+//               strokeLinejoin="round"
+//               strokeWidth={2}
+//               d="M6 18L18 6M6 6l12 12"
+//             />
+//           </svg>
+//         </span>
+//         <form onSubmit={handleSubmit(onSubmit)} className="space-y-5 my-4">
+//           <div>
+//             <label className="text-xl " htmlFor="name">
+//               Name
+//             </label>
+//             <input
+//               className="w-full mt-1 p-3 border-2 border-gray-400 rounded outline-none"
+//               id="name"
+//               defaultValue={user.displayName}
+//               {...register("name", { required: true })}
+//             />
+//           </div>
 
-          <div>
-            <label className="text-xl " htmlFor="email">
-              Email
-            </label>
-            <fieldset disabled>
-              <input
-                type="email"
-                className="w-full mt-1 p-3 border-2 border-gray-400 rounded outline-none bg-gray-300"
-                id="name"
-                defaultValue={user.email}
-                {...register("email", { required: true })}
-                //   disabled
-              />
-            </fieldset>
-          </div>
-          <div>
-            <label className="text-xl " htmlFor="number">
-              Phone Number
-            </label>
-            <input
-              type="number"
-              className="w-full mt-1 p-3 border-2 border-gray-400 rounded outline-none"
-              id="number"
-              {...register("number")}
-              placeholder="Enter Your Number"
-            />
-          </div>
-          <div>
-            <label className="text-xl " htmlFor="address">
-              Address
-            </label>
-            <input
-              className="w-full mt-1 p-3 border-2 border-gray-400 rounded outline-none"
-              id="address"
-              placeholder="Enter Your Address"
-              {...register("address", { required: true })}
-            />
-            <span className="text-red-600 font-medium ">
-              {errors.address && "Address is Required"}
-            </span>
-          </div>
-          <input
-            className=" bg-gray-800 text-white px-8 py-3 rounded inline-block"
-            type="submit"
-            value="Purchase"
-          />
-        </form>
-      </div>
-    </div>
-  );
-};
+//           <div>
+//             <label className="text-xl " htmlFor="email">
+//               Email
+//             </label>
+//             <fieldset disabled>
+//               <input
+//                 type="email"
+//                 className="w-full mt-1 p-3 border-2 border-gray-400 rounded outline-none bg-gray-300"
+//                 id="name"
+//                 defaultValue={user.email}
+//                 {...register("email", { required: true })}
+//                 //   disabled
+//               />
+//             </fieldset>
+//           </div>
+//           <div>
+//             <label className="text-xl " htmlFor="number">
+//               Phone Number
+//             </label>
+//             <input
+//               type="number"
+//               className="w-full mt-1 p-3 border-2 border-gray-400 rounded outline-none"
+//               id="number"
+//               {...register("number")}
+//               placeholder="Enter Your Number"
+//             />
+//           </div>
+//           <div>
+//             <label className="text-xl " htmlFor="address">
+//               Address
+//             </label>
+//             <input
+//               className="w-full mt-1 p-3 border-2 border-gray-400 rounded outline-none"
+//               id="address"
+//               placeholder="Enter Your Address"
+//               {...register("address", { required: true })}
+//             />
+//             <span className="text-red-600 font-medium ">
+//               {errors.address && "Address is Required"}
+//             </span>
+//           </div>
+//           <input
+//             className=" bg-gray-800 text-white px-8 py-3 rounded inline-block"
+//             type="submit"
+//             value="Purchase"
+//           />
+//         </form>
+//       </div>
+//     </div>
+//   );
+// };
 
-export default PurchasePopUp;
+// export default PurchasePopUp;
